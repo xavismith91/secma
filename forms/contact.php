eb871ea79e19d7414ab1875d68fd832ec7d60b29
xavismith91 <xaviersolano91@gmail.com>
2023-07-11 13:06:40 -0600

    fixbutton

---
+++ forms/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+    $nombre =$_POST["nombre"];
+    $mensaje=$_POST["message"];
+    $correo = $_POST["email"];
+    $correo2 = 'sistemas@ce2000.mx';
+    $remitente="SECMA SEGURIDAD";
+    $asunto=$_POST["subject"];
+    $cuerpo="
+    <html> 
+    <meta charset='UTF-8'>
+    <body>
+      <h3> Cliente: $nombre</h3>
+      <h3> Correo: $correo </h3> 
+      <h3> $mensaje </h5>
+      <h4>SECMA SEGURIDAD 2023</h4>
+    </body> 
+    </html> 
+    ";
+    $sheader="From:".$remitente."\n";
+    $sheader=$sheader."X-Mailer:PHP/".phpversion()."\n";
+    $sheader=$sheader."Mime-Version: 1.0\n";
+    $sheader=$sheader."Content-Type: text/html; charset=utf-8\n";
+    mail($correo2,$asunto,$cuerpo,$sheader,$mensaje); 
+
+
+      header("Location: ../index.html");
+      
+    
+  
+
+
+?>
+ 
