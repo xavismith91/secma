82648d172036723c8360a0bc04b47c8cd2486fb5
Xavi_Smith <xaviersolano91@gmail.com>
2023-10-09 15:53:06 -0600

    fixmail091023

--- forms/contact.php
+++ forms/contact.php
@@ -5,15 +5,15 @@
     $correo = $_POST["email"];
     $correo2 = 'contacto@secmaseguridad.com.mx';
     $remitente="SECMA SEGURIDAD";
+    $telefono = $_POST["telefono"];
     $asunto=$_POST["subject"];
-    $telefono=$_POST["telefono"];
     $cuerpo="
     <html> 
     <meta charset='UTF-8'>
     <body>
       <h3> Cliente: $nombre</h3>
       <h3> Correo: $correo </h3> 
-      <h3> Teléfono: $telefono</h3>
+      <h3> Teléfono: $telefono </h3>
       <h3> $mensaje </h5>
       <h4>SECMA SEGURIDAD 2023</h4>
     </body> 
@@ -23,7 +23,7 @@
     $sheader=$sheader."X-Mailer:PHP/".phpversion()."\n";
     $sheader=$sheader."Mime-Version: 1.0\n";
     $sheader=$sheader."Content-Type: text/html; charset=utf-8\n";
-    mail($correo2,$asunto,$cuerpo,$sheader,$mensaje,$telefono); 
+    mail($correo2,$asunto,$telefono,$cuerpo,$sheader,$mensaje); 
 
 
       header("Location: ../index.html");
