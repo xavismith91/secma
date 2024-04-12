2a679322cd469011a86b4cf078732f80ab83f5c1
Xavier Solano <xaviersolano91@gmail.com>
2024-04-12 13:48:54 -0600

    updatecontact

--- forms/contact.php
+++ forms/contact.php
@@ -23,11 +23,14 @@
     $sheader=$sheader."X-Mailer:PHP/".phpversion()."\n";
     $sheader=$sheader."Mime-Version: 1.0\n";
     $sheader=$sheader."Content-Type: text/html; charset=utf-8\n";
-    mail($correo2,$asunto,$cuerpo,$sheader,$mensaje); 
+   
+    if (mail($correo2,$asunto,$cuerpo,$sheader,$mensaje)) {
+      echo '<script>alert("Su Cita ha sido enviada correctamente, espere su confirmación");</script>';
+    }else {
+      echo '<script>alert("Verifique su información e intente de nuevo");</script>';
+    }
 
-
-      header("Location: ../index.html");
-      
+    echo '<script>window.location.href = "../index.html";</script>';
     
   
 
